c27a451f5178991c816bdc1136bc823d4ab764a5
mbeta <martinezbetania@gmail.com>
2014-09-09 08:57:28 -0300

    entidades Pedido Elemento, Tipo de Compra

---
+++ src/App/ComprasBundle/Entity/Pedido_elemento.php
@@ -0,0 +1,274 @@
+<?php
+
+namespace App\ComprasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pedido_elemento
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="App\ComprasBundle\Entity\Pedido_elementoRepository")
+ */
+class Pedido_elemento
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_pedido", type="date")
+     */
+    private $fechaPedido;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="referencia", type="string", length=500)
+     */
+    private $referencia;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="nro_pedido", type="integer")
+     */
+    private $nroPedido;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="observacion", type="string", length=500)
+     */
+    private $observacion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nro_actuacion", type="string", length=255)
+     */
+    private $nroActuacion;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="autorizado", type="boolean")
+     */
+    private $autorizado;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ley", type="string", length=255)
+     */
+    private $ley;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha_autorizado", type="date")
+     */
+    private $fechaAutorizado;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fechaPedido
+     *
+     * @param \DateTime $fechaPedido
+     * @return Pedido_elemento
+     */
+    public function setFechaPedido($fechaPedido)
+    {
+        $this->fechaPedido = $fechaPedido;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaPedido
+     *
+     * @return \DateTime 
+     */
+    public function getFechaPedido()
+    {
+        return $this->fechaPedido;
+    }
+
+    /**
+     * Set referencia
+     *
+     * @param string $referencia
+     * @return Pedido_elemento
+     */
+    public function setReferencia($referencia)
+    {
+        $this->referencia = $referencia;
+
+        return $this;
+    }
+
+    /**
+     * Get referencia
+     *
+     * @return string 
+     */
+    public function getReferencia()
+    {
+        return $this->referencia;
+    }
+
+    /**
+     * Set nroPedido
+     *
+     * @param integer $nroPedido
+     * @return Pedido_elemento
+     */
+    public function setNroPedido($nroPedido)
+    {
+        $this->nroPedido = $nroPedido;
+
+        return $this;
+    }
+
+    /**
+     * Get nroPedido
+     *
+     * @return integer 
+     */
+    public function getNroPedido()
+    {
+        return $this->nroPedido;
+    }
+
+    /**
+     * Set observacion
+     *
+     * @param string $observacion
+     * @return Pedido_elemento
+     */
+    public function setObservacion($observacion)
+    {
+        $this->observacion = $observacion;
+
+        return $this;
+    }
+
+    /**
+     * Get observacion
+     *
+     * @return string 
+     */
+    public function getObservacion()
+    {
+        return $this->observacion;
+    }
+
+    /**
+     * Set nroActuacion
+     *
+     * @param string $nroActuacion
+     * @return Pedido_elemento
+     */
+    public function setNroActuacion($nroActuacion)
+    {
+        $this->nroActuacion = $nroActuacion;
+
+        return $this;
+    }
+
+    /**
+     * Get nroActuacion
+     *
+     * @return string 
+     */
+    public function getNroActuacion()
+    {
+        return $this->nroActuacion;
+    }
+
+    /**
+     * Set autorizado
+     *
+     * @param boolean $autorizado
+     * @return Pedido_elemento
+     */
+    public function setAutorizado($autorizado)
+    {
+        $this->autorizado = $autorizado;
+
+        return $this;
+    }
+
+    /**
+     * Get autorizado
+     *
+     * @return boolean 
+     */
+    public function getAutorizado()
+    {
+        return $this->autorizado;
+    }
+
+    /**
+     * Set ley
+     *
+     * @param string $ley
+     * @return Pedido_elemento
+     */
+    public function setLey($ley)
+    {
+        $this->ley = $ley;
+
+        return $this;
+    }
+
+    /**
+     * Get ley
+     *
+     * @return string 
+     */
+    public function getLey()
+    {
+        return $this->ley;
+    }
+
+    /**
+     * Set fechaAutorizado
+     *
+     * @param \DateTime $fechaAutorizado
+     * @return Pedido_elemento
+     */
+    public function setFechaAutorizado($fechaAutorizado)
+    {
+        $this->fechaAutorizado = $fechaAutorizado;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaAutorizado
+     *
+     * @return \DateTime 
+     */
+    public function getFechaAutorizado()
+    {
+        return $this->fechaAutorizado;
+    }
+}
---
+++ src/App/ComprasBundle/Entity/Tipo_compra.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\ComprasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tipo_compra
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="App\ComprasBundle\Entity\Tipo_compraRepository")
+ */
+class Tipo_compra
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descipcion", type="string", length=255)
+     */
+    private $descipcion;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="monto_min", type="float")
+     */
+    private $montoMin;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="monto_max", type="float")
+     */
+    private $montoMax;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descipcion
+     *
+     * @param string $descipcion
+     * @return Tipo_compra
+     */
+    public function setDescipcion($descipcion)
+    {
+        $this->descipcion = $descipcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descipcion
+     *
+     * @return string 
+     */
+    public function getDescipcion()
+    {
+        return $this->descipcion;
+    }
+
+    /**
+     * Set montoMin
+     *
+     * @param float $montoMin
+     * @return Tipo_compra
+     */
+    public function setMontoMin($montoMin)
+    {
+        $this->montoMin = $montoMin;
+
+        return $this;
+    }
+
+    /**
+     * Get montoMin
+     *
+     * @return float 
+     */
+    public function getMontoMin()
+    {
+        return $this->montoMin;
+    }
+
+    /**
+     * Set montoMax
+     *
+     * @param float $montoMax
+     * @return Tipo_compra
+     */
+    public function setMontoMax($montoMax)
+    {
+        $this->montoMax = $montoMax;
+
+        return $this;
+    }
+
+    /**
+     * Get montoMax
+     *
+     * @return float 
+     */
+    public function getMontoMax()
+    {
+        return $this->montoMax;
+    }
+}
