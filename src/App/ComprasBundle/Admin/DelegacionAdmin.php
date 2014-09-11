b8ece4ad4089a8ff75fb33e465b9ca4bff0a7208
root <root@tupa>
2014-09-11 10:13:18 -0300

    Instalando Sonata 2

---
+++ src/App/ComprasBundle/Admin/DelegacionAdmin.php
@@ -0,0 +1,36 @@
+<?php
+// src/Acme/DemoBundle/Admin/PostAdmin.php
+
+namespace App\ComprasBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class DelegacionAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('descripcion')            
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('descripcion')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('descripcion')
+        ;
+    }
+}
